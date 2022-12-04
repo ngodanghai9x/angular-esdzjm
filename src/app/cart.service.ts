d51b329ec51fc165096a786acbd806e46e264d12
Hai Ngo <ngodanghai9x@gmail.com>
2022-12-04 22:35:27 +0700

    Retrieve shipping prices by http

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,12 +1,17 @@
-import { Product } from './products';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import { Product } from './products';
+
 /* . . . */
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
+  constructor(private http: HttpClient) {}
+
   items: Product[] = [];
-  
+
   addToCart(product: Product) {
     this.items.push(product);
   }
@@ -18,5 +23,9 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>('/assets/shipping.json');
   }
 }
